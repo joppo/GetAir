fd9aa1d43c490a9aff1d09d377b7f98ac93f37d8
joppo <zhivkogospodinov@gmail.com>
2020-01-13 12:36:10 +0000

    insert things added for measurements

--- GetAir/IO/InsertData.cs
+++ GetAir/IO/InsertData.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace GetAir.IO
 {
-    class InsertData
+    public class InsertData
     {
+        public int InsertMeasurement()
+        {
+            int result = -1;
+            using (SqlConnection c = new SqlConnection(Settings.connString))
+            {
+                SqlCommand cmd = new SqlCommand(Queries.q_InsertMeasurements, c);
+                SqlParameter p_time = cmd.Parameters.Add()
+            }
+        }
 
+        //public int InsertCourse(string c_name)
+        //{
+        //    int result = -1;
+        //    using (SqlConnection c = new SqlConnection(Common.Utils.GetConnectionString()))
+        //    {
+        //        SqlCommand cmd = new SqlCommand(Constants.ins_Course, c);
+        //        SqlParameter p_course_name = cmd.Parameters.Add(Constants.p_Name, SqlDbType.NVarChar);
+        //        p_course_name.Value = c_name;
+
+        //        c.Open();
+        //        result = cmd.ExecuteNonQuery();
+        //    }
+        //    return result;
+        //}
     }
 }
--- GetAir/IO/Queries.cs
+++ GetAir/IO/Queries.cs
@@ -7,5 +7,20 @@ namespace GetAir.IO
     class Queries
     {
         public static string q_GetStations = "SELECT Id, Name, url ,DisplayName, Description FROM Stations";
+        public static string q_InsertMeasurements = "INSERT INTO[dbo].[Measurements] ([Time], [CO], [Humidity], [NO2], [O3], [PM10], [PM2], [Pressure], [SO2], [Temperature], [Station_id]) VALUES(@Time, @CO, @Humidity, @NO2, @O3, @MP10, @PM2, @Pressure, @SO2, @Temperature, @Station_id)";
+
+        #region Parameters
+        public static string p_Time = "@Time";
+        public static string p_CO = "@CO";
+        public static string p_Humidity = "@Humidity";
+        public static string NO2 = "@NO2";
+        public static string O3 = "@O3";
+        public static string MP10 = "@MP10";
+        public static string MP2 = "@MP2";
+        public static string Pressure = "@Pressure";
+        public static string SO2 = "@SO2";
+
+
+        #endregion
     }
 }
